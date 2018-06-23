54672bfb5297378c00e87abfbdaf769d294b60f1
Sara S <ssantmier@gmail.com>
2018-06-23 19:10:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ function logShout(string) {return console.log(string.toUpperCase()
 
 function logWhisper(string) {return console.log(string.toLowerCase());}
 
-function sayHiToGrandma(string) {if (string.toLowerCase() === string) {return "I can\'t hear you!";} else if (string.toUpperCase() === string) {return console.log("YES INDEED!");} else if (string === "I love you, Grandma.") {return "I love you, too.";}}
+function sayHiToGrandma(string) {if (string.toLowerCase() === string) {return "I can\'t hear you!";} else if (string.toUpperCase() === string) {return "YES INDEED!";} else if (string === "I love you, Grandma.") {return "I love you, too.";}}
